c8a53519d1b05f22443f43835d7a26c404299b8d
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-10-30 14:14:12 -0300

    chore: Optimizes font loading with preconnect

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,6 +34,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8"/>
           <link rel="icon" href="favicon.png" type="image/x-icon" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
 
