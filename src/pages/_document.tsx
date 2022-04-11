9968074652da046feda8450d0d6c5855a3d96f99
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2022-04-11 12:34:34 -0300

    chore: upgrade package versions

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,10 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Html, Head, Main, NextScript, DocumentInitialProps } from 'next/document'
+import { ReactNode } from 'react';
 import { ServerStyleSheet } from 'styled-components'
 
 import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
-    }
-  }
-
   render() {
     return (
       <Html lang="pt">
@@ -64,4 +39,31 @@ export default class MyDocument extends Document {
       </Html>
     )
   }
+
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ) as any,
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
 }
